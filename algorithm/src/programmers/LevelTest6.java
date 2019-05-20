c921d78decd3f211c9e953f1a6c6625e550fa25a
TyFace <zxuz34@gmail.com>
2019-05-20 16:37:21 +0900

    가운데 글자 가져오기

---
+++ algorithm/src/programmers/LevelTest6.java
@@ -0,0 +1,38 @@
+package programmers;
+
+/*
+가운데 글자 가져오기
+
+문제 설명
+단어 s의 가운데 글자를 반환하는 함수, solution을 만들어 보세요. 단어의 길이가 짝수라면 가운데 두글자를 반환하면 됩니다.
+
+재한사항
+s는 길이가 1 이상, 100이하인 스트링입니다.
+입출력 예
+s		return
+abcde	c
+qwer	we
+
+*/
+
+public class LevelTest6 {
+	/* 수정본 */
+	public String solution(String s) {
+		return s.substring((s.length() - 1) / 2, s.length() / 2 + 1);
+	}
+
+/*	내 풀이
+ * public String solution(String s) {
+		String answer = "";
+
+		int halfNum = s.length() / 2;
+
+		if (s.length() % 2 == 0) {
+			answer = s.substring(halfNum - 1, halfNum + 1);
+		} else {
+			answer = s.substring(halfNum, halfNum + 1);
+		}
+
+		return answer;
+	}
+*/}
